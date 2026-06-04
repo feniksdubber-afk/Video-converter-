982cfbd82e8665f073d028e4c8766a39e332f6d9
feniksdubber-afk <feniksdubber@gmail.com>
2026-06-04 15:08:02 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "")
+API_ID = int(os.environ.get("TELEGRAM_API_ID", "0"))
+API_HASH = os.environ.get("TELEGRAM_API_HASH", "")
 
 MAX_FILE_SIZE_MB = 2000
 TEMP_DIR = "/tmp/videobot"
